80eddc213dcb4f8f8c5d715baa8f9cd957727dfc
thomp <thompickett@gmail.com>
2017-02-03 19:05:34 -0700

    Create Table.findBy() test

--- test/table-test.js
+++ test/table-test.js
@@ -49,7 +49,7 @@ describe("Table", function() {
   });
 
   describe("find()", function() {
-    it("should find and return correct column by it's id", function() {
+    it("should find and return correct table by it's id", function() {
       let tableOpts1 = { originalName: "users", name: "users", columns: [], references: [], status: { new: false, original: true, modified: false, deleted: false } }
       let tableOpts2 = { originalName: "messages", name: "messages", columns: [], references: [], status: { new: false, original: true, modified: false, deleted: false } }
       let table1 = new Table(tableOpts1)
@@ -57,17 +57,65 @@ describe("Table", function() {
       assert.deepEqual(table2, Table.find(102));
     });
 
-    it("should not be able to find column that is deleted", function() {
+    it("should not be able to find table that is deleted", function() {
       let tableOpts1 = { originalName: "users", name: "users", columns: [], references: [], status: { new: false, original: true, modified: false, deleted: true } }
       let table1 = new Table(tableOpts1)
       // Table.delete(101)
       assert.deepEqual(false, Table.find(101));
     });
 
-    it("should return false if column can't be found", function() {
+    it("should return false if table can't be found", function() {
       assert.deepEqual(false, Table.find(101));
     });
 
   });
+
+  describe("findBy()", function() {
+
+    it("should find and return correct table when passed param, name, and it's value", function() {
+      let tableOpts1 = { originalName: "users", name: "users", columns: [], references: [], status: { new: false, original: true, modified: false, deleted: false } }
+      let table1 = new Table(tableOpts1)
+
+      assert.deepEqual([table1], Table.findBy("name", "users"));
+    });
+
+    it("should find and return correct table when passed param, originalName, and it's value", function() {
+      let tableOpts1 = { originalName: "users", name: "users", columns: [], references: [], status: { new: false, original: true, modified: false, deleted: false } }
+      let table1 = new Table(tableOpts1)
+
+      assert.deepEqual([table1], Table.findBy("originalName", "users"));
+    });
+
+    it("shouldn't find correct table when passed param, 'originalName' if all columns, 'originalName' are set to false", function() {
+      let options1 = {table_id: "101", name: "name", type: "integer", original_type: "integer", status: { new: false, original: true, modified: true, deleted: false } };
+      let table = new Table(options1);
+      assert.deepEqual(false, Table.findBy("originalName", false));
+    });
+
+    it("should find and return correct table when passed param, 'id' and it's value", function() {
+      let options1 = {table_id: "101", name: "name", type: "integer", original_type: "integer", status: { new: false, original: true, modified: true, deleted: false } };
+      let table1 = new Table(options1);
+
+      assert.deepEqual([table1], Table.findBy("id", table1.id));
+    });
+
+    it("should return false if param is 'status'", function() {
+      let options1 = {table_id: "101", name: "name", type: "integer", original_type: "integer", status: { new: false, original: true, modified: true, deleted: false } };
+      let table = new Table(options1);
+      assert.deepEqual(false, Table.findBy("status", table.status));
+    });
+
+    it("should return false if table can't be found", function() {
+      assert.deepEqual([], Table.findBy("name","name"));
+    });
+
+    it("should return false if param is invalid", function() {
+      let options1 = {table_id: "101", original_name: "password", name: "password", type: "string", status: { new: false, original: true, modified: false, deleted: false } };
+      let table1 = new Table(options1);
+      assert.deepEqual([], Table.findBy("goop","name"));
+    });
+
+  });
+
 
 });
